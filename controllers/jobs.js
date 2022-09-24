0c729684c99c74a64d5809e53ed46346eb3888fe
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-09-24 22:33:58 +0100

    feat: create basic route and controller Rotes and route controllers created for Jobs(registering and logging in) path API

---
+++ controllers/jobs.js
@@ -0,0 +1,23 @@
+const getAllJobs = async (req,res) => {
+  res.send('all jobs')
+}
+const getJob = async (req,res) => {
+
+}
+const createJob = async (req,res) => {
+
+}
+const updateJob = async (req,res) => {
+
+}
+const deleteJob = async (req,res) => {
+
+}
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+}
