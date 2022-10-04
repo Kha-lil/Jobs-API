5a75ff25cce9b07789439f7dd75fdc39b4dc0435
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-10-05 00:02:35 +0100

    chore: finished up API

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');  
+
 const express = require('express');
 const app = express();
 
 // connect DB
 const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 
 //roters
 const authRoutes = require('./routes/auth')
@@ -14,12 +22,24 @@ const jobRoutes = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
 app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/jobs', jobRoutes)
+app.use('/api/v1/jobs', authenticateUser, jobRoutes)
 
 
 app.use(notFoundMiddleware);
