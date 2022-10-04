3d2c1f5a4843c1dbdce5632d54674acf75e56213
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-10-04 23:49:20 +0100

    feat: add mongoose middlewares and custom methods

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,5 +24,23 @@ const UserSchema = new mongoose.Schema({
     minlength: 6,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.createJWT = function () {
+  const token = jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  return token;
+};
+
+UserSchema.methods.confirmPassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password)
+  return isMatch
+}
 
 module.exports = mongoose.model("User", UserSchema);
